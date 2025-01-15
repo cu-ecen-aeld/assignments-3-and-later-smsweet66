87d28a60b53bcee684f716a7f9860eaec763d45f
Sean Sweet <sweet.sean42@gmail.com>
2025-01-14 20:09:00 -0600

    added username literal to function and conf file, and added comparison between the two

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -14,9 +14,7 @@
 */
 void test_validate_my_username()
 {
-    /**
-     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
-     * config file and my_username() functions are setup properly
-     */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char* usernameLiteral = my_username();
+    const char* usernameConf = malloc_username_from_conf_file();
+    TEST_ASSERT_TRUE_MESSAGE(strncmp(usernameLiteral, usernameConf, strlen(usernameLiteral)) == 0,"AESD students, please fix me!");
 }
